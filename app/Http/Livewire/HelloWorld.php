0484ff20fa1f8ba72012b5fa1581afc7135c6b36
Jesus Rojas <jarojas6524@misena.edu.co>
2022-04-01 01:15:02 -0500

    Data Binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name = 'Rojas';
+    public $loud = false;
+    public $greeting = ['Hello'];
+
     public function render()
     {
         return view('livewire.hello-world', [
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,10 @@
 <div>
-    Hola {{ $name }}
+    <input type="text" wire:model="name">
+    <input type="checkbox" wire:model="loud">
+    <select wire:model="greeting" multiple> 
+        <option>Hello</option>
+        <option>Good Bye</option>
+        <option>See you</option>
+    </select>
+    {{ implode(', ', $greeting) }} {{ $name }} @if($loud) ! @endif
 </div>
